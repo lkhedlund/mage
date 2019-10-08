711d24e853f590a915aadbb684a24a601f92dffc
Lars Hedlund <lkhedlund@gmail.com>
2019-10-08 15:50:49 -0600

    Bootstrap grid set up for pages/posts.

--- base.php
+++ base.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 <div class="wrap" role="document">
-    <main class="main">
-        <?php include Mage\template_path(); ?>
-    </main><!-- /.main -->
-    <?php if (Mage\display_sidebar()) : ?>
-        <aside class="sidebar">
-            <?php get_sidebar() ?>
-        </aside><!-- /.sidebar -->
-    <?php endif; ?>
+    <div class="content">
+        <main class="main">
+            <?php include Mage\template_path(); ?>
+        </main><!-- /.main -->
+        <?php if (Mage\display_sidebar()) : ?>
+            <aside class="sidebar">
+                <?php get_sidebar() ?>
+            </aside><!-- /.sidebar -->
+        <?php endif; ?>
+    </div>
 </div><!-- /.wrap -->
 <?php get_footer(); ?>
