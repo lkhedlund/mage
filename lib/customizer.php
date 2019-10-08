5eb15dabd73c1530922bd8c45fc4033735cf627d
Lars Hedlund <lkhedlund@gmail.com>
2019-10-08 15:38:12 -0600

    Add site logo to customizer control.

--- lib/customizer.php
+++ lib/customizer.php
@@ -2,10 +2,18 @@
 
 namespace Mage;
 
+use WP_Customize_Image_Control;
+
 /**
  * Add postMesmage support
  */
 function customize_register($wp_customize) {
-  $wp_customize->get_setting('blogname')->transport = 'postMesmage';
+  $wp_customize->add_setting('upload_site_logo');
+
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'upload_site_logo', array(
+        'label' => 'Logo',
+        'section' => 'title_tagline',
+        'settings' => 'upload_site_logo',
+    )));
 }
 add_action('customize_register', __NAMESPACE__ . '\\customize_register');
--- templates/navigation.php
+++ templates/navigation.php
@@ -1,4 +1,4 @@
-<nav class="navbar fixed-top navbar-expand-md navbar-light bg-light" role="navigation" aria-label="main navigation">
+<nav class="navbar fixed-top navbar-expand-md navbar-light bg-light" role="navigation">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
