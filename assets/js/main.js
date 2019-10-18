6f83d11f7bffe172ccbf887b8f18bd76f6d9f2b7
Lars Hedlund <lkhedlund@gmail.com>
2019-10-18 08:53:29 -0600

    Remove drawer javascript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,3 @@
 (function($) {
-
-    function toggleEl(el, classname = 'open') {
-        if (el.hasClass(classname)) {
-            el.removeClass(classname);
-        } else {
-            el.addClass(classname);
-        }
-    }
-
-    $('.button-menu').click(function(e) {
-        e.preventDefault();
-        const el = $(this),
-            id = el.attr('data-target'),
-            drawer = $(`#${ id }`);
-
-        toggleEl(drawer);
-        toggleEl(el, 'is-active');
-    });
-
-    $(document).on('click', function(event) {
-        var target = $(event.target);
-        var drawer = target.closest('.drawer').length;
-        var menu = target.closest('#menu').length;
-        if ( !drawer  && !menu ) {
-            toggleEl(drawer);
-            toggleEl(menu, 'is-active');
-        }
-    });
-    // END
+    // reference jQuery as $ within.
 })(jQuery);
